569f1c51c7bb8957319d3b2bb5e721440c3b0bb3
moroznastya <nastya.moroz2210@gmail.com>
2022-05-23 20:41:36 +0300

    added lab 9

---
+++ main.py
@@ -0,0 +1,8 @@
+from transport.transport import Auto, Car, Truck, Tram, Trolleybus
+
+if __name__ == '__main__':
+    auto = Auto("bmw", 120)
+    car = Car("kia", 100, "sportage")
+    truck = Truck(40, "renault", 90)
+    tram = Tram("VinWay", "СтрийськийРинок")
+    trolleybus = Trolleybus("Електрон", "Хуторівка")
